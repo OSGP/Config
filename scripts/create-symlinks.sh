8c343736e3dcebd619b43d1384f626d32f24cbe1
Kevin Smeets <kevin.smeets@cgi.com>
2015-04-30 15:42:02 +0200

    Adds some scripts and fixes missing property.

---
+++ scripts/create-symlinks.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# Create symlinks to app config files.
+dir="/home/dev/Sources/Config/apps"
+target="/etc/osp"
+for f in "$dir"/*; do
+  sudo ln -s "$f" "$target"
+  echo "$f"
+done
+
+# Create symlinks to logback config files.
+dir="/home/dev/Sources/Config/logging"
+for f in "$dir"/*; do
+  sudo ln -s "$f" "$target"
+  echo "$f"
+done
+
+# Create symlinks to device simulator ECDSA keypair.
+sudo ln -s /home/dev/Sources/Config/certificates/oslp/oslp_sim_ecdsa_private.der /etc/ssl/certs
+sudo ln -s /home/dev/Sources/Config/certificates/oslp/oslp_sim_ecdsa_public.der /etc/ssl/certs
+
+# Create symlinks to platform ECDSA keypair.
+sudo ln -s /home/dev/Sources/Config/certificates/oslp/oslp_test_ecdsa_private.der /etc/ssl/certs
+sudo ln -s /home/dev/Sources/Config/certificates/oslp/oslp_test_ecdsa_public.der /etc/ssl/certs
+
+
+# Create symlink to CA certificate.
+sudo ln -s /home/dev/Sources/Config/certificates/osgp-ca/certs/cacert.cer /etc/ssl/certs
+
+# Create symlink to server certificate.
+sudo ln -s /home/dev/Sources/Config/certificates/osgp-ca/certs/localhost.cert.pem /etc/ssl/certs
+
+# Create symlink to organization certificate and personal information exchange.
+sudo ln -s /home/dev/Sources/Config/certificates/osgp-ca/certs/test-org.cert.pem /etc/ssl/certs
+sudo ln -s /home/dev/Sources/Config/certificates/osgp-ca/certs/test-org.pfx /etc/ssl/certs
+
+
+# Create symlink to server private key.
+sudo ln -s /home/dev/Sources/Config/certificates/osgp-ca/private/localhost.key.pem /etc/ssl/private
+
+# Create symlink to organization private key.
+sudo ln -s /home/dev/Sources/Config/certificates/osgp-ca/private/test-org.key.pem /etc/ssl/private
+
+
+# Create symlink to keystore.
+sudo ln -s /home/dev/Sources/Config/certificates/trust.jks /etc/ssl/certs
+
+
+# Create symlink to maven settings.
+sudo ln -s /home/dev/Sources/Config/maven/settings.xml /home/dev/.m2
--- sql/create-users-and-databases.sql
+++ sql/create-users-and-databases.sql
@@ -1,6 +1,6 @@
 -- Create the admin user
 
-CREATE ROLE db_admin LOGIN
+CREATE ROLE osp_admin LOGIN
   PASSWORD '1234'
   SUPERUSER INHERIT CREATEDB CREATEROLE REPLICATION;
 
@@ -9,56 +9,35 @@ CREATE ROLE db_admin LOGIN
 -- Create the databases
 
 CREATE DATABASE osgp_adapter_protocol_oslp
-  WITH OWNER = db_admin
+  WITH OWNER = osp_admin
        ENCODING = 'UTF8'
        TABLESPACE = pg_default
        CONNECTION LIMIT = -1;
 
 
 CREATE DATABASE osgp_core
-  WITH OWNER = db_admin
+  WITH OWNER = osp_admin
        ENCODING = 'UTF8'
        TABLESPACE = pg_default
        CONNECTION LIMIT = -1;
 
 
 CREATE DATABASE osgp_ws_logging
-  WITH OWNER = db_admin
+  WITH OWNER = osp_admin
        ENCODING = 'UTF8'
        TABLESPACE = pg_default
        CONNECTION LIMIT = -1;
 
 
 CREATE DATABASE osp_devicesimulator_web
-  WITH OWNER = db_admin
+  WITH OWNER = osp_admin
        ENCODING = 'UTF8'
        TABLESPACE = pg_default
        CONNECTION LIMIT = -1;
 
 
 CREATE DATABASE osp_logging
-  WITH OWNER = db_admin
-       ENCODING = 'UTF8'
-       TABLESPACE = pg_default
-       CONNECTION LIMIT = -1;
-
-
-CREATE DATABASE osp_net_management_web
-  WITH OWNER = db_admin
-       ENCODING = 'UTF8'
-       TABLESPACE = pg_default
-       CONNECTION LIMIT = -1;
-
-
-CREATE DATABASE osp_owner_web
-  WITH OWNER = db_admin
-       ENCODING = 'UTF8'
-       TABLESPACE = pg_default
-       CONNECTION LIMIT = -1;
-
-
-CREATE DATABASE osp_user_management_web
-  WITH OWNER = db_admin
+  WITH OWNER = osp_admin
        ENCODING = 'UTF8'
        TABLESPACE = pg_default
        CONNECTION LIMIT = -1;
@@ -74,61 +53,3 @@ GRANT SELECT ON ALL TABLES IN SCHEMA public TO osgp_read_only_ws_user;
 
 CREATE USER osgp_core_db_api_user PASSWORD '1234' NOSUPERUSER;
 GRANT SELECT ON public.device TO osgp_core_db_api_user;
-
--- Create the logback databases
-
--- Logback: the reliable, generic, fast and flexible logging framework.
--- Copyright (C) 1999-2010, QOS.ch. All rights reserved.
---
--- See http://logback.qos.ch/license.html for the applicable licensing 
--- conditions.
-
--- This SQL script creates the required tables by ch.qos.logback.classic.db.DBAppender
---
--- It is intended for PostgreSQL databases.
-
--- DROP TABLE    logging_event_property;
--- DROP TABLE    logging_event_exception;
--- DROP TABLE    logging_event;
--- DROP SEQUENCE logging_event_id_seq;
-
-
-CREATE SEQUENCE logging_event_id_seq MINVALUE 1 START 1;
-
-
-CREATE TABLE logging_event 
-  (
-    timestmp         BIGINT NOT NULL,
-    formatted_message  TEXT NOT NULL,
-    logger_name       VARCHAR(254) NOT NULL,
-    level_string      VARCHAR(254) NOT NULL,
-    thread_name       VARCHAR(254),
-    reference_flag    SMALLINT,
-    arg0              VARCHAR(254),
-    arg1              VARCHAR(254),
-    arg2              VARCHAR(254),
-    arg3              VARCHAR(254),
-    caller_filename   VARCHAR(254) NOT NULL,
-    caller_class      VARCHAR(254) NOT NULL,
-    caller_method     VARCHAR(254) NOT NULL,
-    caller_line       CHAR(4) NOT NULL,
-    event_id          BIGINT DEFAULT nextval('logging_event_id_seq') PRIMARY KEY
-  );
-
-CREATE TABLE logging_event_property
-  (
-    event_id	      BIGINT NOT NULL,
-    mapped_key        VARCHAR(254) NOT NULL,
-    mapped_value      VARCHAR(1024),
-    PRIMARY KEY(event_id, mapped_key),
-    FOREIGN KEY (event_id) REFERENCES logging_event(event_id)
-  );
-
-CREATE TABLE logging_event_exception
-  (
-    event_id         BIGINT NOT NULL,
-    i                SMALLINT NOT NULL,
-    trace_line       VARCHAR NOT NULL,
-    PRIMARY KEY(event_id, i),
-    FOREIGN KEY (event_id) REFERENCES logging_event(event_id)
-  );
