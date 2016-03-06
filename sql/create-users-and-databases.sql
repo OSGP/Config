420ecc8fd00f4aae38865429f2571aaa1e0f7ce5
Kevin Smeets <kevin.smeets@cgi.com>
2016-03-06 13:31:57 +0100

    FLEX-457 ~ Updates database creation script.

--- sql/create-users-and-databases.sql
+++ sql/create-users-and-databases.sql
@@ -12,6 +12,12 @@ CREATE DATABASE osgp_adapter_protocol_oslp
        TABLESPACE = pg_default
        CONNECTION LIMIT = -1;
 
+CREATE DATABASE osgp_adapter_protocol_dlms
+  WITH OWNER = osp_admin
+       ENCODING = 'UTF8'
+       TABLESPACE = pg_default
+       CONNECTION LIMIT = -1;
+
 CREATE DATABASE osgp_core
   WITH OWNER = osp_admin
        ENCODING = 'UTF8'
@@ -24,13 +30,12 @@ CREATE DATABASE osp_devicesimulator_web
        TABLESPACE = pg_default
        CONNECTION LIMIT = -1;
 
-
 CREATE DATABASE osgp_logging
   WITH OWNER = osp_admin
        ENCODING = 'UTF8'
        TABLESPACE = pg_default
        CONNECTION LIMIT = -1;
-	   
+
 -- Create the readonly users
 
 CREATE USER osgp_read_only_ws_user WITH PASSWORD '1234' NOSUPERUSER;
