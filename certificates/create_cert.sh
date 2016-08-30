eb8ed54ea57be142f5b77db4aeab9f21640dfe91
The "dev" user <dev@dev-box>
2016-08-30 11:26:00 +0200

    Added new Test Organisation certificate for automatic tests

--- certificates/create_cert.sh
+++ certificates/create_cert.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 
 REQUESTS=osgp-ca/requests
 PRIVATE=osgp-ca/private
@@ -14,14 +14,14 @@ export OPENSSL_CONF=osgp-ca.cfg
 echo ########################################################################
 echo # Create certificate request: $REQUESTS/$COMMON_NAME.req.pem
 echo ########################################################################
-openssl req -new -sha256 -reqexts $CERT_TYPE -nodes -out $REQUESTS/$COMMON_NAME.req.pem -keyout $PRIVATE/$COMMON_NAME.key.pem -subj /commonName=$COMMON_NAME
+openssl req -config $OPENSSL_CONF -new -sha256 -reqexts $CERT_TYPE -nodes -out "$REQUESTS/$COMMON_NAME.req.pem" -keyout "$PRIVATE/$COMMON_NAME.key.pem" -subj /commonName="$COMMON_NAME"
 
 echo ########################################################################
 echo # Sign certificate request: $CERTS/$COMMON_NAME.cert.pem
 echo ########################################################################
-openssl ca -days 1095 -md sha256 -extensions $CERT_TYPE -batch -passin pass:$CA_PASS -out $CERTS/$COMMON_NAME.cert.pem -infiles $REQUESTS/$COMMON_NAME.req.pem
+openssl ca -config $OPENSSL_CONF -days 1095 -md sha256 -extensions $CERT_TYPE -batch -passin pass:$CA_PASS -out "$CERTS/$COMMON_NAME.cert.pem" -infiles "$REQUESTS/$COMMON_NAME.req.pem"
 
 echo ########################################################################
 echo # Create PKCS12 file: $CERTS/$COMMON_NAME.pfx
 echo ########################################################################
-openssl pkcs12 -export -inkey $PRIVATE/$COMMON_NAME.key.pem -in $CERTS/$COMMON_NAME.cert.pem -certfile $CERTS/cacert.cer -out $CERTS/$COMMON_NAME.pfx -passout pass:$PKCS12_PASS
+openssl pkcs12 -export -inkey "$PRIVATE/$COMMON_NAME.key.pem" -in "$CERTS/$COMMON_NAME.cert.pem" -certfile $CERTS/cacert.cer -out "$CERTS/$COMMON_NAME.pfx" -passout pass:$PKCS12_PASS
