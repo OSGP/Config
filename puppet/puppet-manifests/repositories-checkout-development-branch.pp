10dada743bbcba3cc3fc009459c9d8be749bb15c
Martijn Sips <martijn.sips@cgi.com>
2016-09-16 08:48:32 +0200

    Updated additional configuration and test projects

--- puppet/puppet-manifests/repositories-checkout-development-branch.pp
+++ puppet/puppet-manifests/repositories-checkout-development-branch.pp
@@ -3,32 +3,42 @@
 node 'dev-box' {
 
 	exec { 'Config repo':
-		command => '/bin/sh -c "cd /home/dev/Sources/Config; /usr/bin/git checkout development"',
+		command => '/bin/sh -c "cd /home/dev/Sources/OSGP/Config; /usr/bin/git checkout development"',
 		returns => [0,128],
 	}
 
 	exec { 'Shared repo':
-		command => '/bin/sh -c "cd /home/dev/Sources/Shared; /usr/bin/git checkout development"',
+		command => '/bin/sh -c "cd /home/dev/Sources/OSGP/Shared; /usr/bin/git checkout development"',
 		returns => [0,128],
 	}
 
 	exec { 'Platform repo':
-		command => '/bin/sh -c "cd /home/dev/Sources/Platform; /usr/bin/git checkout  development"',
+		command => '/bin/sh -c "cd /home/dev/Sources/OSGP/Platform; /usr/bin/git checkout  development"',
 		returns => [0,128],
 	}
 
 	exec { 'Protocol-Adapter-OSLP repo':
-		command => '/bin/sh -c "cd /home/dev/Sources/Protocol-Adapter-OSLP; /usr/bin/git checkout development"',
+		command => '/bin/sh -c "cd /home/dev/Sources/OSGP/Protocol-Adapter-OSLP; /usr/bin/git checkout development"',
 		returns => [0,128],
 	}
 
 	exec { 'Protocol-Adapter-DLMS repo':
-		command => '/bin/sh -c "cd /home/dev/Sources/Protocol-Adapter-DLMS; /usr/bin/git checkout development"',
+		command => '/bin/sh -c "cd /home/dev/Sources/OSGP/Protocol-Adapter-DLMS; /usr/bin/git checkout development"',
 		returns => [0,128],
 	}
 	
+	exec { 'Protocol-Adapter-IEC61850 repo':
+		command => '/bin/sh -c "cd /home/dev/Sources/OSGP/Protocol-Adapter-IEC61850; /usr/bin/git checkout development"',
+		returns => [0,128],
+	}
+
 	exec { 'Documentation repo':
-		command => '/bin/sh -c "cd /home/dev/Sources/Documentation; /usr/bin/git checkout development"',
+		command => '/bin/sh -c "cd /home/dev/Sources/OSGP/Documentation; /usr/bin/git checkout development"',
+		returns => [0,128],
+	}
+
+	exec { 'Tests repo':
+		command => '/bin/sh -c "cd /home/dev/Sources/OSGP/Integration-Tests; /usr/bin/git checkout development"',
 		returns => [0,128],
 	}
 
--- scripts/create-symlinks.sh
+++ scripts/create-symlinks.sh
@@ -1,57 +1,57 @@
 #!/bin/bash
 
 # Create symlinks to app config files.
-dir="/home/dev/Sources/Config/apps"
+dir="/home/dev/Sources/OSGP/Config/apps"
 target="/etc/osp"
 for f in "$dir"/*; do
-  sudo ln -s "$f" "$target"
+  sudo ln -sf "$f" "$target"
   echo "$f"
 done
 
 # Create symlinks to logback config files.
-dir="/home/dev/Sources/Config/logging"
+dir="/home/dev/Sources/OSGP/Config/logging"
 for f in "$dir"/*; do
-  sudo ln -s "$f" "$target"
+  sudo ln -sf "$f" "$target"
   echo "$f"
 done
 
 # Create symlinks to device simulator ECDSA keypair.
-sudo ln -s /home/dev/Sources/Config/certificates/oslp/oslp_sim_ecdsa_private.der /etc/ssl/certs
-sudo ln -s /home/dev/Sources/Config/certificates/oslp/oslp_sim_ecdsa_public.der /etc/ssl/certs
+sudo ln -sf /home/dev/Sources/OSGP/Config/certificates/oslp/oslp_sim_ecdsa_private.der /etc/ssl/certs
+sudo ln -sf /home/dev/Sources/OSGP/Config/certificates/oslp/oslp_sim_ecdsa_public.der /etc/ssl/certs
 
 # Create symlinks to platform ECDSA keypair.
-sudo ln -s /home/dev/Sources/Config/certificates/oslp/oslp_test_ecdsa_private.der /etc/ssl/certs
-sudo ln -s /home/dev/Sources/Config/certificates/oslp/oslp_test_ecdsa_public.der /etc/ssl/certs
+sudo ln -sf /home/dev/Sources/OSGP/Config/certificates/oslp/oslp_test_ecdsa_private.der /etc/ssl/certs
+sudo ln -sf /home/dev/Sources/OSGP/Config/certificates/oslp/oslp_test_ecdsa_public.der /etc/ssl/certs
 
 
 # Create symlink to CA certificate.
-sudo ln -s /home/dev/Sources/Config/certificates/osgp-ca/certs/cacert.cer /etc/ssl/certs
+sudo ln -sf /home/dev/Sources/OSGP/Config/certificates/osgp-ca/certs/cacert.cer /etc/ssl/certs
 
 # Create symlink to server certificate.
-sudo ln -s /home/dev/Sources/Config/certificates/osgp-ca/certs/localhost.cert.pem /etc/ssl/certs
+sudo ln -sf /home/dev/Sources/OSGP/Config/certificates/osgp-ca/certs/localhost.cert.pem /etc/ssl/certs
 
 # Create symlink to organization certificate and personal information exchange.
-sudo ln -s /home/dev/Sources/Config/certificates/osgp-ca/certs/test-org.cert.pem /etc/ssl/certs
-sudo ln -s /home/dev/Sources/Config/certificates/osgp-ca/certs/test-org.pfx /etc/ssl/certs
+sudo ln -sf /home/dev/Sources/OSGP/Config/certificates/osgp-ca/certs/test-org.cert.pem /etc/ssl/certs
+sudo ln -sf /home/dev/Sources/OSGP/Config/certificates/osgp-ca/certs/test-org.pfx /etc/ssl/certs
 
 
 # Create symlink to server private key.
-sudo ln -s /home/dev/Sources/Config/certificates/osgp-ca/private/localhost.key.pem /etc/ssl/private
+sudo ln -sf /home/dev/Sources/OSGP/Config/certificates/osgp-ca/private/localhost.key.pem /etc/ssl/private
 
 # Create symlink to organization private key.
-sudo ln -s /home/dev/Sources/Config/certificates/osgp-ca/private/test-org.key.pem /etc/ssl/private
+sudo ln -sf /home/dev/Sources/OSGP/Config/certificates/osgp-ca/private/test-org.key.pem /etc/ssl/private
 
 
 # Create symlink to keystore.
-sudo ln -s /home/dev/Sources/Config/certificates/trust.jks /etc/ssl/certs
+sudo ln -sf /home/dev/Sources/OSGP/Config/certificates/trust.jks /etc/ssl/certs
 
 
 # Create symlink to apache vhost and remove the link to the 000-default.conf vhost.
-sudo ln -s /home/dev/Sources/Config/apache-httpd/vhost.conf /etc/apache2/sites-enabled
+sudo ln -sf /home/dev/Sources/OSGP/Config/apache-httpd/vhost.conf /etc/apache2/sites-enabled
 sudo unlink /etc/apache2/sites-enabled/000-default.conf
 sudo service apache2 restart
 
 # Create symlink to Maven settings
-sudo mkdir /home/dev/.m2
-sudo ln -s /home/dev/Sources/Config/maven/settings.xml /home/dev/.m2
+sudo mkdir -p /home/dev/.m2
+sudo ln -sf /home/dev/Sources/OSGP/Config/maven/settings.xml /home/dev/.m2
 sudo chown -R dev:dev /home/dev/.m2
