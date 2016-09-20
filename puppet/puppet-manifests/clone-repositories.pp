d7a5aab6ff768d00fca7672b2ae77be9bc951036
pczeeuw <pieter.zeeuw@cgi.com>
2016-09-20 13:31:03 +0000

    Clone sources into Sources/OSGP, increased credential helper to 86400

--- puppet/puppet-manifests/clone-repositories.pp
+++ puppet/puppet-manifests/clone-repositories.pp
@@ -3,35 +3,35 @@
 node 'dev-box' {
 
 	exec { 'cloning Config repo':
-		command => '/usr/bin/git clone https://github.com/OSGP/Config.git /home/dev/Sources/Config',
+		command => '/usr/bin/git clone https://github.com/OSGP/Config.git /home/dev/Sources/OSGP/Config',
 	}
 
 	exec { 'cloning Shared repo':
-		command => '/usr/bin/git clone https://github.com/OSGP/Shared.git /home/dev/Sources/Shared',
+		command => '/usr/bin/git clone https://github.com/OSGP/Shared.git /home/dev/Sources/OSGP/Shared',
 	}
 
 	exec { 'cloning Platform repo':
-		command => '/usr/bin/git clone https://github.com/OSGP/Platform.git /home/dev/Sources/Platform',
+		command => '/usr/bin/git clone https://github.com/OSGP/Platform.git /home/dev/Sources/OSGP/Platform',
 	}
 
 	exec { 'cloning Protocol-Adapter-OSLP repo':
-		command => '/usr/bin/git clone https://github.com/OSGP/Protocol-Adapter-OSLP.git /home/dev/Sources/Protocol-Adapter-OSLP',
+		command => '/usr/bin/git clone https://github.com/OSGP/Protocol-Adapter-OSLP.git /home/dev/Sources/OSGP/Protocol-Adapter-OSLP',
 	}
 
 	exec { 'cloning Protocol-Adapter-DLMS repo':
-		command => '/usr/bin/git clone https://github.com/OSGP/Protocol-Adapter-DLMS.git /home/dev/Sources/Protocol-Adapter-DLMS',
+		command => '/usr/bin/git clone https://github.com/OSGP/Protocol-Adapter-DLMS.git /home/dev/Sources/OSGP/Protocol-Adapter-DLMS',
 	}
 	
 	exec { 'cloning Protocol-Adapter-IEC61850 repo':
-		command => '/usr/bin/git clone https://github.com/OSGP/Protocol-Adapter-IEC61850.git /home/dev/Sources/Protocol-Adapter-IEC61850',
+		command => '/usr/bin/git clone https://github.com/OSGP/Protocol-Adapter-IEC61850.git /home/dev/Sources/OSGP/Protocol-Adapter-IEC61850',
 	}
 
 	exec { 'cloning Tests repo':
-		command => '/usr/bin/git clone https://github.com/OSGP/Integration-Tests.git /home/dev/Sources/Integration-Tests',
+		command => '/usr/bin/git clone https://github.com/OSGP/Integration-Tests.git /home/dev/Sources/OSGP/Integration-Tests',
 	}
 
 	exec { 'cloning Documentation repo':
-		command => '/usr/bin/git clone https://github.com/OSGP/Documentation.git /home/dev/Sources/Documentation',
+		command => '/usr/bin/git clone https://github.com/OSGP/Documentation.git /home/dev/Sources/OSGP/Documentation',
 	}
 
 }
--- puppet/puppet-manifests/create-source-code-folder.pp
+++ puppet/puppet-manifests/create-source-code-folder.pp
@@ -3,7 +3,7 @@
 node 'dev-box' {
 
 	exec { 'creating source code folder':
-		command => '/bin/mkdir /home/dev/Sources',
+		command => '/bin/mkdir -p /home/dev/Sources/OSGP',
 		before => Exec['chown source code folder'],	
 	}
 	
@@ -13,7 +13,7 @@ node 'dev-box' {
 	}
 
 	exec { 'chown source code folder':
-		command => '/bin/chown dev:dev /home/dev/Sources',
+		command => '/bin/chown dev:dev /home/dev/Sources/OSGP',
 	}
 	
 	exec { 'chown osgp download folder':
