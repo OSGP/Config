dad8b5434f69f1c3c9ceed83e93ba8031da6bc4a
Martijn Sips <martijn.sips@cgi.com>
2016-09-22 09:49:20 +0200

    Updated configuration

--- puppet/manifests/activemq-development.pp
+++ puppet/manifests/activemq-development.pp
@@ -1,6 +1,6 @@
 node 'dev-box' {
 	
         exec { 'Configure activemq for development':
-		command => "/bin/sed -i 's/<broker xmlns=/<broker deleteAllMessagesOnStartup=\"true\" xmlns=/g' /opt/apache-activemq-5.11.1/conf/activemq.xml"
+		command => "/bin/sed -i 's/<broker xmlns=/<broker deleteAllMessagesOnStartup=\"true\" xmlns=/g' /home/dev/Tools/apache-activemq-5.11.1/conf/activemq.xml"
         }
 }
