24fa1790bdc35d8aa0ea90011f7c8221578cfcad
martijnsips <martijn.sips@cgi.com>
2016-09-29 15:41:41 +0200

    Fixed paths

--- puppet/manifests/personalize-desktop.pp
+++ puppet/manifests/personalize-desktop.pp
@@ -3,7 +3,7 @@
 node 'dev-box' {
 
 	exec { 'add osgp wallpaper':
-		command => '/bin/cp -f /home/dev/repos/Config/dev/OSGP-solar.png /usr/share/backgrounds/warty-final-ubuntu.png',
+		command => '/bin/cp -f /home/dev/Sources/Config/dev/OSGP-solar.png /usr/share/backgrounds/warty-final-ubuntu.png',
 	}
 	
 	exec { 'git cache':
@@ -12,20 +12,20 @@ node 'dev-box' {
 	
 	exec { 'add desktop shortcuts':
 		user => 'dev',
-		command => '/bin/sh /home/dev/repos/Config/scripts/create_desktop_shortcuts.sh',
+		command => '/bin/sh /home/dev/Sources/Config/scripts/create_desktop_shortcuts.sh',
 		require => Exec['chmod create_desktop_shortcuts.sh','chmod create_application_shortcut.sh','chmod create_weblink_shortcut.sh'],
 	}
 	
 	exec { 'chmod create_desktop_shortcuts.sh':
-		command => '/bin/chmod a+rwx /home/dev/repos/Config/scripts/create_desktop_shortcuts.sh',
+		command => '/bin/chmod a+rwx /home/dev/Sources/Config/scripts/create_desktop_shortcuts.sh',
 	}
 	
 	exec { 'chmod create_application_shortcut.sh':
-		command => '/bin/chmod a+rwx /home/dev/repos/Config/scripts/create_application_shortcut.sh',
+		command => '/bin/chmod a+rwx /home/dev/Sources/Config/scripts/create_application_shortcut.sh',
 	}
 	
 	exec { 'chmod create_weblink_shortcut.sh':
-		command => '/bin/chmod a+rwx /home/dev/repos/Config/scripts/create_weblink_shortcut.sh',
+		command => '/bin/chmod a+rwx /home/dev/Sources/Config/scripts/create_weblink_shortcut.sh',
 	}
 
 }
--- scripts/create_desktop_shortcuts.sh
+++ scripts/create_desktop_shortcuts.sh
@@ -6,17 +6,17 @@ if [ ! -d "/home/dev/Desktop" ]; then
 fi
 
 echo "### Create shortcuts"
-/home/dev/repos/Config/scripts/create_application_shortcut.sh eclipse false /home/dev/Tools/eclipse/icon.xpm Eclipse /home/dev/Tools/eclipse/eclipse
+/home/dev/Sources/OSGP/Config/scripts/create_application_shortcut.sh eclipse false /home/dev/Tools/eclipse/icon.xpm Eclipse /home/dev/Tools/eclipse/eclipse
 
-/home/dev/repos/Config/scripts/create_application_shortcut.sh activemq true /usr/share/icons/hicolor/48x48/apps/openjdk-7.png ActiveMQ "/home/dev/Tools/apache-activemq-5.14.0/bin/linux-x86-64/activemq console"
+/home/dev/Sources/OSGP/Config/scripts/create_application_shortcut.sh activemq true /usr/share/icons/hicolor/48x48/apps/openjdk-7.png ActiveMQ "/home/dev/Tools/apache-activemq-5.14.0/bin/linux-x86-64/activemq console"
 
-/home/dev/repos/Config/scripts/create_application_shortcut.sh soapui false /usr/share/icons/hicolor/icons/scalable/status/account-logged-in.svg SoapUI /home/dev/Tools/SoapUI-5.2.1/bin/soapui.sh
+/home/dev/Sources/OSGP/Config/scripts/create_application_shortcut.sh soapui false /usr/share/icons/hicolor/icons/scalable/status/account-logged-in.svg SoapUI /home/dev/Tools/SoapUI-5.2.1/bin/soapui.sh
 
-/home/dev/repos/Config/scripts/create_weblink_shortcut.sh github "OSGP Github" "http://www.github.com/osgp/"
+/home/dev/Sources/OSGP/Config/scripts/create_weblink_shortcut.sh github "OSGP Github" "http://www.github.com/osgp/"
 
-/home/dev/repos/Config/scripts/create_weblink_shortcut.sh documentation "OSGP Documentation" "http://documentation.opensmartgridplatform.org/"
+/home/dev/Sources/OSGP/Config/scripts/create_weblink_shortcut.sh documentation "OSGP Documentation" "http://documentation.opensmartgridplatform.org/"
 
-/home/dev/repos/Config/scripts/create_weblink_shortcut.sh devicesim "OSLP Device Simulator" "https://localhost/web-device-simulator/devices"
+/home/dev/Sources/OSGP/Config/scripts/create_weblink_shortcut.sh devicesim "OSLP Device Simulator" "https://localhost/web-device-simulator/devices"
 
-/home/dev/repos/Config/scripts/create_weblink_shortcut.sh osgp "Open Smart Grid Platform website" "http://www.opensmartgridplatform.org/"
+/home/dev/Sources/OSGP/Config/scripts/create_weblink_shortcut.sh osgp "Open Smart Grid Platform website" "http://www.opensmartgridplatform.org/"
 
