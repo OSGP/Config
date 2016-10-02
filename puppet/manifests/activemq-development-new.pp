b57a7810a060cf516f02726fec75115f945fb5f9
Hans Rooden <villgust@gmail.com>
2016-10-02 05:49:36 +0000

    FLEX-214 Deletes old ActiveMQ 5.11 puppet scripts

--- puppet/manifests/activemq-development-new.pp
+++
@@ -1,6 +0,0 @@
-node 'dev-box' {
-	
-        exec { 'Configure activemq for development':
-		command => "/bin/sed -i 's/<broker xmlns=/<broker deleteAllMessagesOnStartup=\"true\" xmlns=/g' /opt/apache-activemq-5.11.1/conf/activemq.xml"
-        }
-}
