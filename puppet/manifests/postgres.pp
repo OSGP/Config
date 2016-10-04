0945ed668495243eb6424abe6ac28f0384aa8e42
Martijn Sips <martijn.sips@cgi.com>
2016-10-04 12:46:36 +0200

    Updated postgresql.pp

--- puppet/manifests/postgres.pp
+++ puppet/manifests/postgres.pp
@@ -3,20 +3,20 @@
 node 'dev-box' {
 
 	# PostgreSQL.
-	package { 'postgresql-9.3':
+	package { 'postgresql':
 		ensure => installed,
 	}
 
 	service { 'postgresql':
 		ensure => 'running',
 		enable => true,
-		require => Package['postgresql-9.3'],
+		require => Package['postgresql'],
 	}
 
 	exec { 'Increase connections postgres':
-		command => '/bin/sed -i \'s/max_connections = .*$/max_connections = 1000 /\' /etc/postgresql/9.3/main/postgresql.conf',
+		command => '/bin/sed -i \'s/max_connections = .*$/max_connections = 1000 /\' /etc/postgresql/9.*/main/postgresql.conf',
 		returns => [0, 4],
-		require => Package['postgresql-9.3'],
+		require => Package['postgresql'],
 		notify => Service['postgresql'],
 	}
 }
