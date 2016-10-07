5c7d0826f4f92504e42b7c4fe7b8f823b6583810
Frank Heijnders <frank.heijnders@cgi.com>
2016-10-07 20:25:50 +0200

    fixes path to shutdown command

--- puppet/manifests/restart-vm.pp
+++ puppet/manifests/restart-vm.pp
@@ -1,7 +1,7 @@
 node 'dev-box' {
 
 	exec { 'Restart': 
-		command => 'shutdown -r +1',
+		command => '/sbin/shutdown -r +1',
 		require => Exec['Restarting VM  in 1min to activate several settings']
 	}
 }
