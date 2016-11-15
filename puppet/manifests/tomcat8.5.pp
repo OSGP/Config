10c957e8c1af4901efebe34a64859eb6bdfa200b
The "dev" user <dev@dev-box>
2016-11-15 11:34:19 +0100

    Updated tomcat8.5.pp to 8.5.8

--- puppet/manifests/tomcat8.5.pp
+++ puppet/manifests/tomcat8.5.pp
@@ -2,26 +2,28 @@
 
 node 'dev-box' {
 
+	$version = "8.5.8"
+
 	# Tomcat7 is used as application server.
 	exec { 'wget tomcat 8.5':
-		command => '/usr/bin/wget -q -P /home/dev/Downloads/osgp - http://apache.mirrors.spacedump.net/tomcat/tomcat-8/v8.5.6/bin/apache-tomcat-8.5.6.tar.gz',
+		command => "/usr/bin/wget -q -P /home/dev/Downloads/osgp - http://apache.mirrors.spacedump.net/tomcat/tomcat-8/v$version/bin/apache-tomcat-$version.tar.gz",
 		before => Exec['wget postgresql jdbc','unpack tomcat','change permissions of tomcat conf files'],	
 		returns => [0, 4],
 	}
 
 	exec { 'unpack tomcat':
-		command => '/bin/tar xzf /home/dev/Downloads/osgp/apache-tomcat-8.5.6.tar.gz -C /home/dev/Tools',
+		command => "/bin/tar xzf /home/dev/Downloads/osgp/apache-tomcat-$version.tar.gz -C /home/dev/Tools",
 		before => Exec['wget postgresql jdbc','change permissions of tomcat conf files'],
 	}
 
 	exec { 'change permissions of tomcat conf files':
-		command => '/bin/chmod 644 /home/dev/Tools/apache-tomcat-8.5.6/conf/*',
+		command => "/bin/chmod 644 /home/dev/Tools/apache-tomcat-$version/conf/*",
 	}
 
 	file { 'create tomcat link in tools':
 		path => '/home/dev/Tools/tomcat',
 		ensure => link,
-		target => '/home/dev/Tools/apache-tomcat-8.5.6'
+		target => "/home/dev/Tools/apache-tomcat-$version"
 	}
 
 	# A JDBC is needed by Tomcat to connect to PostgreSQL.
