5ae5017e9795fecf3218e4bccb353a6bdc801aa2
Martijn Sips <martijn.sips@cgi.com>
2017-01-02 08:38:50 +0000

    Updates soapui 5.2.1 -> 5.3.0

--- puppet/manifests/soapui.pp
+++ puppet/manifests/soapui.pp
@@ -2,16 +2,18 @@
 
 node 'dev-box' {
 
+	$version='5.3.0';
+
 	# Soap-UI
 	exec { 'wget soap-ui':
-		command => '/usr/bin/wget -q -O /home/dev/Downloads/osgp/SoapUI-5.2.1-linux-bin.tar.gz http://cdn01.downloads.smartbear.com/soapui/5.2.1/SoapUI-5.2.1-linux-bin.tar.gz',
+		command => "/usr/bin/wget -q -O /home/dev/Downloads/osgp/SoapUI-${version}-linux-bin.tar.gz http://cdn01.downloads.smartbear.com/soapui/${version}/SoapUI-${version}-linux-bin.tar.gz",
 		onlyif => '/usr/bin/test ! -f /home/dev/Tools/SoapUI',
 		timeout => 1800,		
 		returns => [0, 4],
 	}
 
 	exec { 'unpack soap-ui':
-		command => '/bin/tar xzf /home/dev/Downloads/osgp/SoapUI-5.2.1-linux-bin.tar.gz -C /home/dev/Tools',
+		command => "/bin/tar xzf /home/dev/Downloads/osgp/SoapUI-${version}-linux-bin.tar.gz -C /home/dev/Tools",
 		onlyif => '/usr/bin/test ! -f /home/dev/Tools/SoapUI',
 		require => Exec['wget soap-ui']
 	}
@@ -19,7 +21,7 @@ node 'dev-box' {
 	file { 'create soapui link':
 		ensure => link,
 		path => '/home/dev/Tools/SoapUI',
-		target => '/home/dev/Tools/SoapUI-5.2.1',
+		target => "/home/dev/Tools/SoapUI-${version}",
 		require => Exec['unpack soap-ui']
 	}
 
