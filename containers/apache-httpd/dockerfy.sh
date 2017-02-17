e7808596ffbb1c4efbd40ff0070b32e12645c4bc
Kevin Smeets <kevin.smeets@cgi.com>
2017-02-17 11:01:27 +0000

    Adapts logging for Apache vhost.

--- containers/apache-httpd/dockerfy.sh
+++ containers/apache-httpd/dockerfy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 COMPONENT=apache-httpd
 docker build -t $COMPONENT .
-docker run -it --rm -e "SOAPENDPOINT=172.17.0.6" -p 8099:80 -p 44399:443 --name apache-httpd $COMPONENT
+docker run -it --rm -e "SOAPENDPOINT=172.17.0.2" -p 8099:80 -p 44399:443 --name $COMPONENT $COMPONENT
