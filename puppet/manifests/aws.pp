6f74ad7569e06b5d27941b62a9c93d81f2977484
The "dev" user <dev@dev-box>
2017-03-18 22:22:20 +0000

    Updates

--- puppet/manifests/aws.pp
+++ puppet/manifests/aws.pp
@@ -1,17 +1,29 @@
 node 'dev-box' {
 
-        package { 'awscli':
-                ensure => installed
-        }
+	$homedir="/home/dev"
 
+        package { 'awscli':
+                ensure => present
+        }
 
 	class { 'python':
-		pip => present
-		
+		pip => present	
 	}
 
-        exec { 'awsudo':
-                command => '/usr/bin/pip install --user git+https://github.com/paperg/awsudo.git',
+	exec { '/usr/bin/pip install --upgrade pip':
 		require => Class['python'] 
         }
+
+        exec { 'Install awsudo':
+		command => '/usr/bin/pip install --user git+https://github.com/paperg/awsudo.git',
+		user => 'dev',
+		require => Exec['/usr/bin/pip install --upgrade pip'] 
+        }
+
+        exec { "Add awsudo to path in ${homedir}.profile":
+                command => "/bin/sed -i 's/:\$PATH/:\\/home\\/dev\\/.local\\/bin:\$PATH/g' ${homedir}/.profile",
+		unless => "/bin/grep PATH= /.profile | /bin/grep .local\\/bin", 
+		require => Exec['Install awsudo'], 
+        }
 }
+
