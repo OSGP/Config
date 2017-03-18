33c48d018c068500f1988bd09286688f65a56656
The "dev" user <dev@dev-box>
2017-03-18 14:59:02 +0000

    Updated

--- puppet/manifests/intellij.pp
+++ puppet/manifests/intellij.pp
@@ -2,37 +2,38 @@
 
 node 'dev-box' {
 
+        $homedir="/home/dev"
         $version="2016.3.5"
 
         # IntellIJ
 	exec { 'Download IntellIJ':
-		command => "/usr/bin/wget -q -O /home/dev/Downloads/osgp/ideaIC-${version}.tar.gz https://download.jetbrains.com/idea/ideaIC-${version}.tar.gz",
-		creates => "/home/dev/Downloads/osgp/ideaIC-${version}.tar.gz",
+		command => "/usr/bin/wget -q -O ${homedir}/Downloads/ideaIC-${version}.tar.gz https://download.jetbrains.com/idea/ideaIC-${version}.tar.gz",
+		creates => "${homedir}/Downloads/ideaIC-${version}.tar.gz",
 		timeout => 1800,		
 		returns => [0, 4],
 	}
 
 	exec { 'Extract IntellIJ':
-		command => "/bin/mkdir -p /home/dev/Tools/ideaIC-${version} && /bin/tar xzf /home/dev/Downloads/osgp/ideaIC-${version}.tar.gz -C /home/dev/Tools/ideaIC-${version}",
-		onlyif => "/usr/bin/test ! -d /home/dev/Tools/ideaIC-${version}",
+		command => "/bin/mkdir -p ${homedir}/Tools/ideaIC-${version} && /bin/tar xzf ${homedir}/Downloads/ideaIC-${version}.tar.gz -C ${homedir}/Tools/ideaIC-${version}",
+		onlyif => "/usr/bin/test ! -d ${homedir}/Tools/ideaIC-${version}",
 		require => Exec['Download IntellIJ'], 
 	}
 
         exec { 'Move inner idea-IC dir':
-		command => "/bin/mv /home/dev/Tools/ideaIC-${version}/idea-IC-*/* /home/dev/Tools/ideaIC-${version} && /bin/rm -rf /home/dev/Tools/ideaIC-${version}/idea-IC-*",
-		onlyif => "/usr/bin/test -d /home/dev/Tools/ideaIC-${version}/idea-IC-*",
+		command => "/bin/mv ${homedir}/Tools/ideaIC-${version}/idea-IC-*/* ${homedir}/Tools/ideaIC-${version} && /bin/rm -rf ${homedir}/Tools/ideaIC-${version}/idea-IC-*",
+		onlyif => "/usr/bin/test -d ${homedir}/Tools/ideaIC-${version}/idea-IC-*",
 		require => Exec['Extract IntellIJ']
 	}
 
 	file { 'Create IntellIJ link':
 		ensure => link,
-		path => '/home/dev/Tools/ideaIC',
-		target => "/home/dev/Tools/ideaIC-${version}",
+		path => "${homedir}/Tools/ideaIC",
+		target => "${homedir}/Tools/ideaIC-${version}",
 		require => Exec['Move inner idea-IC dir']
 	}
 
         file { 'Create destop link':
-                path => '/home/dev/Desktop/intellij.desktop',
+                path => "${homedir}/Desktop/intellij.desktop",
                 ensure => present,
                 mode => 755,
                 content => '[Desktop Entry]
