4772842084c3a6249ec0820cd4533751c1283786
Martijn Sips <martijn.sips@cgi.com>
2017-04-20 04:33:44 +0000

    Added info and addition git pull and git fetch commands

--- scripts/pull_development_and_update_submodules.sh
+++ scripts/pull_development_and_update_submodules.sh
@@ -1,27 +1,54 @@
 #!/bin/sh
 
+echo ">>> Shared ..."
 cd Shared
+git fetch
+git pull
 git pull origin development
+echo ">>> Platform ..."
 cd ../Platform
+git fetch
+git pull
 git pull origin development
 git submodule update --remote
+echo ">>> Protocol-Adapter-OSLP ..."
 cd ../Protocol-Adapter-OSLP
+git fetch
+git pull
 git pull origin development
 git submodule update --remote
+echo ">>> Protocol-Adapter-DLMS ..."
 cd ../Protocol-Adapter-DLMS
+git fetch
+git pull
 git pull origin development
 git submodule update --remote
+echo ">>> Protocol-Adapter-IEC61850 ..."
 cd ../Protocol-Adapter-IEC61850
+git fetch
+git pull
 git pull origin development
 git submodule update --remote
+echo ">>> Integration-Tests ..."
 cd ../Integration-Tests
+git fetch
+git pull
 git pull origin development
 git submodule update --remote
+echo ">>> Config ..."
 cd ../Config
+git fetch
+git pull
 git pull origin development
+echo ">>> Documentation ..."
 cd ../Documentation 
+git fetch
+git pull
 git pull origin development
+echo ">>> PublicLightingDemoApp ..."
 cd ../PublicLightingDemoApp 
+git fetch
+git pull
 git pull origin development
 git submodule update --remote
 cd ..
