f319eed3f3a0d28cf12d820471d9ef07b6ce165f
Rianne Fijn <rianne.fijn@cgi.com>
2018-02-07 11:39:00 +0100

    SLIM-941 precommit hook and symlink script

---
+++ scripts/symlink_git_hooks.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+base=$HOME
+
+# If no HOME directory was set, use a BASE directory that is /home/dev
+if [ ! $base ]
+then
+    base=/home/dev
+fi
+
+source_directory=$base/Sources/OSGP
+target_directory=*/.git/hooks
+
+cd $source_directory
+git_hook_directories=$(ls -d $TARGETDIR)
+
+for directory in $git_hook_directories;
+do
+	echo symlinking the pre-commit hook to $directory
+	ln -sf $source_directory/Config/git_hooks/pre-commit $directory 
+done
