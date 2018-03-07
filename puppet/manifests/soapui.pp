06c3d5f5f28d9d92c35f4977381acbdb95e59b52
Karl <karl.booklover@gmail.com>
2018-03-07 11:02:26 -0300

    Updated broken SoapUI download links
    the original SoapUI links (http://cdn01.downloads.smartbear.com/soapui/5.3.0/SoapUI-5.3.0-linux-bin.tar.gz) no longer work. I inserted the new link which is: https://s3.amazonaws.com/downloads.eviware/soapuios/5.4.0/SoapUI-x64-5.4.0.sh

--- puppet/manifests/soapui.pp
+++ puppet/manifests/soapui.pp
@@ -2,30 +2,29 @@
 
 node 'dev-box' {
 
-	$version='5.3.0'
+	$version='5.4.0'
 
 	# Soap-UI
 	exec { 'wget soap-ui':
-		command => "/usr/bin/wget -q -O /home/dev/Downloads/osgp/SoapUI-${version}-linux-bin.tar.gz http://cdn01.downloads.smartbear.com/soapui/${version}/SoapUI-${version}-linux-bin.tar.gz",
-		onlyif => '/usr/bin/test ! -f /home/dev/Tools/SoapUI',
-		timeout => 1800,		
+		command => "/usr/bin/wget -q -O /home/dev/Tools/SoapUI http://cdn01.downloads.smartbear.com/soapui/${version}/SoapUI-${version}-linux-bin.tar.gz",
+		onlyif => '/usr/bin/test ! -f /home/dev/Tools/SoapUI',																			https://s3.amazonaws.com/downloads.eviware/soapuios/${version}/SoapUI-x64-${version}.sh
+		timeout => 1800,
 		returns => [0, 4],
 	}
-	
 
-	exec { 'unpack soap-ui':
-		command => "/bin/tar xzf /home/dev/Downloads/osgp/SoapUI-${version}-linux-bin.tar.gz -C /home/dev/Tools",
-		onlyif => '/usr/bin/test ! -f /home/dev/Tools/SoapUI',
-		require => Exec['wget soap-ui']
-	}
+#	exec { 'unpack soap-ui':
+#		command => "/bin/tar xzf /home/dev/Downloads/osgp/SoapUI-${version}-linux-bin.tar.gz -C /home/dev/Tools",
+#		onlyif => '/usr/bin/test ! -f /home/dev/Tools/SoapUI',
+#		require => Exec['wget soap-ui']
+#	}
 
 	file { 'create soapui link':
 		ensure => link,
 		path => '/home/dev/Tools/SoapUI',
 		target => "/home/dev/Tools/SoapUI-${version}",
-		require => Exec['unpack soap-ui']
+		require => Exec['wget soap-ui']
 	}
-	
+
 	exec { 'wget icon':
 		command => "/usr/bin/wget -q -O /home/dev/Tools/SoapUI/logo.png http://www.daxiongmao.eu/wiki/images/1/19/Icon_soapui.png",
                 onlyif => '/usr/bin/test ! -f /home/dev/Tools/SoapUI/logo.png',
