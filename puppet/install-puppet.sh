f586d3049aa592fe81e2b46b8473c3e24537cb07
Wout van Heeswijk <wout.van.heeswijk@smartsocietyservices.com>
2018-03-20 11:12:23 +0100

    -  add the use of DPkg::Options::=--force-confdef to apt-get to    make it configure using the defaults. This fixes the "vagrant up"    error: "dpkg-reconfigure: unable to re-open stdin: No file or     directory" - Consistantly use ";" in Vagrant file. - Redirect stderr to stdout for git. This makes the git clone command   appear green on screen.

--- puppet/install-puppet.sh
+++ puppet/install-puppet.sh
@@ -10,11 +10,11 @@ echo "preparing puppet.deb file";
 sudo dpkg -i ~/Downloads/puppet-release-xenial.deb
 
 echo "running apt-get update";
-sudo apt-get update
+sudo apt-get -o DPkg::Options::=--force-confdef update
 
 echo "installing puppet";
-sudo apt-get install -y puppet-common
-sudo apt-get install -y puppet
+sudo apt-get -o DPkg::Options::=--force-confdef install -y puppet-common
+sudo apt-get -o DPkg::Options::=--force-confdef install -y puppet
 
 echo "running puppet --version";
 echo $(puppet --version)
