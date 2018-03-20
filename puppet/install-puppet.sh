6fefc29064b9012a465f216ab4699827829c7419
Wout van Heeswijk <wout.van.heeswijk@smartsocietyservices.com>
2018-03-20 11:57:48 +0100

    trying to fix false errors with vragrant up

--- puppet/install-puppet.sh
+++ puppet/install-puppet.sh
@@ -3,7 +3,6 @@
 # Run this script as your user, not as root!
 # The script will prompt for root password (for the first sudo command) and will prompt if you want to continue with installation of packages (for the apt-get install command).
 
-export DEBIAN_FRONTEND=noninteractive
 echo "downloading puppet.deb file to ~/Downloads folder";
 wget -q -P ~/Downloads - https://apt.puppetlabs.com/puppet-release-xenial.deb
 
@@ -11,10 +10,10 @@ echo "preparing puppet.deb file";
 sudo dpkg -i ~/Downloads/puppet-release-xenial.deb
 
 echo "running apt-get update";
-sudo apt-get -o DPkg::Options::=--force-confdef update
+sudo apt-get -qqo DPkg::Options::=--force-confdef update
 
 echo "installing puppet";
-sudo apt-get -o DPkg::Options::=--force-confdef install -y puppet puppet-common
+sudo apt-get -qqo DPkg::Options::=--force-confdef install -y puppet puppet-common
 
 echo "running puppet --version";
 echo $(puppet --version)
