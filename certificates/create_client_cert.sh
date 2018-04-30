ca43796b72c7600f4671a76201727b0b694a17cd
Ruud Lemmers <ruud.lemmers@cgi.com>
2018-04-30 09:52:28 +0000

    Renews test-org and localhost certificates

--- certificates/create_client_cert.sh
+++ certificates/create_client_cert.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+# Description
+# Call create_cert.sh with specific client certificate configuration section "v3_client".
+
+# Params for the script.
+# $1 = the common name (CN) and the file name for the certificate
+# $2 = CA password
+# $3 = client cert password
 ./create_cert.sh v3_client "$1" "$2" "$3"
