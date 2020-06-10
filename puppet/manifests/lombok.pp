40a5bbc330f7c6b12a38f835c1cba6eafaaa570a
JelleHoffman <jelle.hoffman@hotmail.com>
2020-06-10 15:20:10 +0000

    Added lombok to run puppet script and updated the lombok script

--- puppet/manifests/lombok.pp
+++ puppet/manifests/lombok.pp
@@ -1,17 +1,21 @@
 node 'dev-box' {
 	
 	exec { 'wget lombok':
-		command => "/usr/bin/wget -q -O /home/dev/Downloads/osgp/lombok.jar https://projectlombok.org/downloads/lombok.jar",
-		creates => "/home/dev/Downloads/osgp/lombok.jar",
+		command => "/usr/bin/wget -q -O /home/dev/Downloads/lombok.jar https://projectlombok.org/downloads/lombok.jar",
+		creates => "/home/dev/Downloads/lombok.jar",
 		timeout => 1800,
 		returns => [0, 4],
 	}
 
 	exec { 'cp lombok':
-		command => "/bin/cp /home/dev/Downloads/osgp/lombok.jar /home/dev/Tools/eclipse/",
+		command => "/bin/cp /home/dev/Downloads/lombok.jar /home/dev/Tools/eclipse/",
 		onlyif => "/usr/bin/test -L /home/dev/Tools/eclipse",
 		require => Exec["wget lombok"],
 	}
 
+	exec { 'add lombok line':
+		command => '/bin/grep -qxF "-javaagent:/home/dev/Tools/eclipse/lombok.jar" /home/dev/Tools/eclipse/eclipse.ini || /bin/echo "-javaagent:/home/dev/Tools/eclipse/lombok.jar" >> /home/dev/Tools/eclipse/eclipse.ini',
+		require => Exec['cp lombok'],
+	}
 	
 }
