618ff1230d016d5ba5b4e1b8e6d1f695b4132832
Kevin Smeets <kevin.smeets@alliander.com>
2022-12-13 13:20:04 +0100

    FDP-866 ~ Fix SoapUI logo.
    Signed-off-by: Kevin Smeets <kevin.smeets@alliander.com>

--- puppet/manifests/soapui.pp
+++ puppet/manifests/soapui.pp
@@ -8,10 +8,10 @@ node 'dev-box' {
 	exec { 'wget soap-ui':
 		command => "/usr/bin/wget -q -O /home/dev/Downloads/osgp/SoapUI-${version}-linux-bin.tar.gz http://s3.amazonaws.com/downloads.eviware/soapuios/${version}/SoapUI-${version}-linux-bin.tar.gz",
 		onlyif => '/usr/bin/test ! -f /home/dev/Tools/SoapUI',
-		timeout => 1800,		
+		timeout => 1800,
 		returns => [0, 4],
 	}
-	
+
 
 	exec { 'unpack soap-ui':
 		command => "/bin/tar xzf /home/dev/Downloads/osgp/SoapUI-${version}-linux-bin.tar.gz -C /home/dev/Tools",
@@ -25,10 +25,10 @@ node 'dev-box' {
 		target => "/home/dev/Tools/SoapUI-${version}",
 		require => Exec['unpack soap-ui']
 	}
-	
+
 	exec { 'wget icon':
-		command => "/usr/bin/wget -q -O /home/dev/Tools/SoapUI/logo.png http://www.daxiongmao.eu/wiki/images/1/19/Icon_soapui.png",
-                onlyif => '/usr/bin/test ! -f /home/dev/Tools/SoapUI/logo.png',
+		command => "/usr/bin/wget -q -O /home/dev/Tools/SoapUI/logo.png https://symbols.getvecta.com/stencil_25/77_soapui.5113237b76.png",
+		onlyif => '/usr/bin/test ! -f /home/dev/Tools/SoapUI/logo.png',
 		require => File['create soapui link']
 	}
 
