3653f92ae47ddcdcafd9997f849fd382c75ac67f
Kevin Smeets <kevin.smeets@alliander.com>
2022-12-13 20:26:31 +0100

    FDP-866 ~ Add dependencies for puppet.
    Signed-off-by: Kevin Smeets <kevin.smeets@alliander.com>

--- puppet/install-puppet.sh
+++ puppet/install-puppet.sh
@@ -3,13 +3,13 @@
 # Run this script as your user, not as root!
 # The script will prompt for root password (for the first sudo command) and will prompt if you want to continue with installation of packages (for the apt-get install command).
 
+sudo apt-get install facter ruby-augeas hiera ruby ruby-deep-merge ruby-shadow ruby-sorted-set
+
 echo "downloading puppet.deb file to ~/Downloads folder";
 wget -q -P ~/Downloads - http://archive.ubuntu.com/ubuntu/pool/universe/p/puppet/puppet_5.5.22-4_all.deb
 
 echo "preparing puppet.deb file";
 sudo dpkg -i ~/Downloads/puppet_5.5.22-4_all.deb
-
-sudo apt-get install hiera
 
 #echo "running apt-get update";
 #sudo DEBIAN_FRONTEND=noninteractive apt-get -o DPkg::Options::=--force-confdef update
